f5d83dc9a95b59aafb9a5c2f9774d2c7ed72a3ec
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-06 21:24:24 +0530

    quiz app finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: QuizApp(),
       ),
     ),
@@ -19,7 +20,39 @@ class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text("Your content here"),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            Text( "This is where your question will go",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 30),textAlign: TextAlign.center,),
+            SizedBox(height: 230,),
+            MaterialButton(
+                color: Colors.green,
+                minWidth: 300,
+                height:80,
+                child:
+                Text('True'),
+
+                onPressed:
+                    (){}
+
+            ),
+            SizedBox(height: 20,),
+            MaterialButton(
+                color: Colors.red,
+                minWidth: 300,
+                height:80,
+                child:
+                Text('False'),
+
+                onPressed:
+                    (){}
+
+            )
+
+          ]
+      ),
+
     );
   }
 }
